075c23cda7c3fbe2beaa3dc700bf0443e6a88e3a
ppetrid <ppetrid@yawd.eu>
2012-10-28 20:23:17 +0200

    removed unused import

--- elfinder/fields.py
+++ elfinder/fields.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.forms import CharField
-from django.core.exceptions import ValidationError
 from connector import ElfinderConnector
 
 class ElfinderFile(object):
