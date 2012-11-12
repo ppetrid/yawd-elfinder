d386a21ecd42caf8301a59f50769a8a6a8df02ee
ppetrid <ppetrid@yawd.eu>
2012-11-12 13:47:58 +0200

    doc version fix

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ copyright = u'2012, yawd'
 # built documents.
 #
 # The short X.Y version.
-version = '0.90.00'
+version = '0.90'
 # The full version, including alpha/beta/rc tags.
 release = '0.90.00'
 
