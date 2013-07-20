18bcf354f61f75fea6dfd5c794ca34ac757753d4
Mo Mughrabi <mo.mughrabi@gmail.com>
2013-07-20 22:40:22 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
         ],
       include_package_data = True,
       install_requires = [
-        "Django>=1.4",
+        "Django>=1.5",
         "PIL",
         "python-magic"
         ],
